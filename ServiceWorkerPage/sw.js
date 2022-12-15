b7a38a8aaaf111f0d4f546e11c1dc1a8dbd6a9f1
Jonas Lauer <jonas.lauer@bizerba.com>
2022-12-15 21:15:29 +0100

    Make service worker notification show all options

--- ServiceWorkerPage/sw.js
+++ ServiceWorkerPage/sw.js
@@ -1,9 +1,4 @@
 self.addEventListener("push", e => {
     const data = e.data.json();
-    self.registration.showNotification(
-        data.title, // title of the notification
-        {
-            body: "Test2", //the body of the push notification
-        }
-    );
+    self.registration.showNotification( data.title, data.options );
 });
