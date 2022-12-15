007dec6862cfeca4d35df2c174c3f4f367bd4728
Jonas Lauer <jonas.lauer@bizerba.com>
2022-12-15 22:11:39 +0100

    Skip waiting when isnstalling

--- ServiceWorkerPage/sw.js
+++ ServiceWorkerPage/sw.js
@@ -2,3 +2,6 @@ self.addEventListener("push", e => {
     const data = e.data.json();
     self.registration.showNotification( data.title, data.options );
 });
+self.addEventListener("install", () => {
+    self.skipWaiting();
+});
